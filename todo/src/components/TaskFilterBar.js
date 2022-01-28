96e9a475f807ed2a54c145b493d7c753a1fc3e36
danilverisokin <verisokindaniil@gmail.com>
2022-01-28 11:14:53 +0300

    Update filter button

--- todo/src/components/TaskFilterBar.js
+++ todo/src/components/TaskFilterBar.js
@@ -1,58 +1,67 @@
 import { FILTER_VARIANTS, SORT_DATE_VARIANTS } from '../constants';
-import { Button } from 'antd';
+import { Radio } from 'antd';
 import { CaretUpOutlined, CaretDownOutlined } from '@ant-design/icons';
 
 const TaskFilterBar = ({ handlleSortByDate, handeFilter }) => {
   return (
     <div className="filterBar">
       <div className="filterButtons">
-        <Button
-          onClick={() => handeFilter(FILTER_VARIANTS.FILTER_ALL)}
-          // className={
-          //   tasksFilter === FILTER_VARIANTS.FILTER_ALL ? 'buttonsMainItemPushed' : 'filterButton'
-          // }
-        >
-          All
-        </Button>
-        <Button
-          onClick={() => handeFilter(FILTER_VARIANTS.FILTER_DONE)}
-          // className={
-          //   tasksFilter === FILTER_VARIANTS.FILTER_DONE ? 'buttonsMainItemPushed' : 'filterButton'
-          // }
-        >
-          Done
-        </Button>
-        <Button
-          onClick={() => handeFilter(FILTER_VARIANTS.FILTER_UNDONE)}
-          // className={
-          //   tasksFilter === FILTER_VARIANTS.FILTER_UNDONE ? 'buttonsMainItemPushed' : 'filterButton'
-          // }
-        >
-          Undone
-        </Button>
+        <Radio.Group defaultValue="a" buttonStyle="solid">
+          <Radio.Button
+            value="a"
+            onClick={() => handeFilter(FILTER_VARIANTS.FILTER_ALL)}
+            // className={
+            //   tasksFilter === FILTER_VARIANTS.FILTER_ALL ? 'buttonsMainItemPushed' : 'filterButton'
+            // }
+          >
+            All
+          </Radio.Button>
+          <Radio.Button
+            value="b"
+            onClick={() => handeFilter(FILTER_VARIANTS.FILTER_DONE)}
+            // className={
+            //   tasksFilter === FILTER_VARIANTS.FILTER_DONE ? 'buttonsMainItemPushed' : 'filterButton'
+            // }
+          >
+            Done
+          </Radio.Button>
+          <Radio.Button
+            value="c"
+            onClick={() => handeFilter(FILTER_VARIANTS.FILTER_UNDONE)}
+            // className={
+            //   tasksFilter === FILTER_VARIANTS.FILTER_UNDONE ? 'buttonsMainItemPushed' : 'filterButton'
+            // }
+          >
+            Undone
+          </Radio.Button>
+        </Radio.Group>
       </div>
       <div className="sortByDateElem">
         <div className="sortByDateText">Sort by date</div>
-        <Button
-          onClick={() => handlleSortByDate(SORT_DATE_VARIANTS.SORT_ASC)}
-          // className={
-          //   sortByDate === SORT_DATE_VARIANTS.SORT_ASC
-          //     ? 'buttonsSortArrowsItemPushed'
-          //     : 'sortButton'
-          // }
-        >
-          <CaretUpOutlined />
-        </Button>
-        <Button
-          onClick={() => handlleSortByDate(SORT_DATE_VARIANTS.SORT_DESC)}
-          // className={
-          //   sortByDate === SORT_DATE_VARIANTS.SORT_DESC
-          //     ? 'buttonsSortArrowsItemPushed'
-          //     : 'sortButton'
-          // }
-        >
-          <CaretDownOutlined />
-        </Button>
+        <Radio.Group defaultValue="b" buttonStyle="solid">
+          <Radio.Button
+            value="a"
+            onClick={() => handlleSortByDate(SORT_DATE_VARIANTS.SORT_ASC)}
+            // className={
+            //   sortByDate === SORT_DATE_VARIANTS.SORT_ASC
+            //     ? 'buttonsSortArrowsItemPushed'
+            //     : 'sortButton'
+            // }
+          >
+            <CaretUpOutlined />
+          </Radio.Button>
+          <Radio.Button
+            value="b"
+            onClick={() => handlleSortByDate(SORT_DATE_VARIANTS.SORT_DESC)}
+            // className={
+            //   sortByDate === SORT_DATE_VARIANTS.SORT_DESC
+            //     ? 'buttonsSortArrowsItemPushed'
+            //     : 'sortButton'
+            // }
+          >
+            <CaretDownOutlined />
+          </Radio.Button>
+        </Radio.Group>
       </div>
     </div>
   );
